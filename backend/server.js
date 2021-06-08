0a4796ac10e5c137c98a273286a98a6fe6a6f89d
Zafar201 <ayshasawaab@gmail.com>
2021-06-08 07:35:56 +0530

    4:00:00

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,22 @@ import data from './data.js';
 
 const app = express();
 
+app.get('/api/products/:id',(req,res)=>{
+    const product = data.products.find((x) =>x._id === req.params.id);
+    if(product){
+       res.send(product);
+    //    console.log('okkk');
+    }else{
+        res.status(404).send({message:"product n not found"})
+        // console.log("noooo");
+    }
+})
+
 app.get('/api/products',(req,res)=>{
     res.send(data.products)
+    // console.log("heyyyy");
 })
+
 
 app.get('/',(req,res)=>{
     res.send('server is running')
