f5ff0970c814231893c048df8088e8d7df8173c2
Zafar201 <ayshasawaab@gmail.com>
2021-07-03 14:21:32 +0530

    publishhh to herokuu

--- backend/server.js
+++ backend/server.js
@@ -32,9 +32,11 @@ app.get('/api/config/paypal',(req,res)=>{
 });
 const __dirname = path.resolve();
 app.use('/upload', express.static(path.join(__dirname, '/upload')));
-app.get('/',(req,res)=>{
-    res.send('server is running')
-});
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req,res) => res.sendFile(path.join(__dirname, '/frontend/build/index.html')));
+// app.get('/',(req,res)=>{
+//     res.send('server is running')
+// });
 
 app.use((err, req, res, next)=>{
     res.status(500).send({message: err.message})
