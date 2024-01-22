c375e0839f0df3b016e0ddcc52e62c71fefa0a7f
Muhammed Najeeb <muhdnajeeb3@gmail.com>
2024-01-22 20:03:23 +0530

    n

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.get('/api/config/paypal',(req,res)=>{
 const __dirname = path.resolve();
 app.use('/upload', express.static(path.join(__dirname, '/upload')));
 app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req,res) => res.sendFile(path.join(__dirname, '/frontend/build/index.html')));
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
 // app.get('/',(req,res)=>{
 //     res.send('server is running')
 // });
