41a6123c35921d8d56a51c49b956e4d029b499f9
Muhammed Najeeb <muhdnajeeb3@gmail.com>
2024-01-23 12:25:57 +0530

    serve changes

--- backend/server.js
+++ backend/server.js
@@ -31,12 +31,14 @@ app.get('/api/config/paypal',(req,res)=>{
     res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
 });
 const __dirname = path.resolve();
-app.use('/upload', express.static(path.join(__dirname, '/upload')));
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req,res) => res.sendFile(path.join(__dirname, '/frontend/build/index.html')));
-// app.get('/',(req,res)=>{
-//     res.send('server is running')
-// });
+// Serve static files from the 'frontend/build' directory
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+// Add the upload directory if needed
+app.use('/upload', express.static(path.join(__dirname, 'upload')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
+  });
 
 app.use((err, req, res, next)=>{
     res.status(500).send({message: err.message})
